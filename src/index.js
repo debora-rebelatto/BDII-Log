84973cbbd624a38520c6e4a98b7e61b4c40c9937
debora-rebelatto <deborarebelatto1@gmail.com>
2022-12-06 18:22:42 -0300

    :sparkles: feat: insere valores iniciais

--- src/index.js
+++ src/index.js
@@ -9,6 +9,27 @@ const port = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', async (req, res) => {
+  try {
+    const res = await getMetadata();
+    return res;
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+});
+
+async function getMetadata() {
+  const selectquery = 'SELECT * FROM metadado';
+
+  return res.send(
+    await client
+      .query(selectquery)
+      .then(res => console.log(res.rows))
+      .catch(e => console.error(e.stack))
+  )
+}
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
@@ -22,47 +43,75 @@ async function readLog() {
     await client.connect();
 
     client.query('DROP TABLE IF EXISTS metadado');
+
     client
       .query('CREATE TABLE IF NOT EXISTS metadado (id VARCHAR(255) PRIMARY KEY, A INTEGER, B INTEGER)')
       .then(res => console.log("ok"))
       .catch(e => console.error(e.stack))
 
+    insertInitialData();
 
-    const idm = metadado.id;
-    const A = metadado.INITIAL.A;
-    const B = metadado.INITIAL.B;
+    lines.forEach(line => {
 
-    var size = Object.keys(metadado.INITIAL).length;
+      const lineArray = line.split(' ');
 
+      // if lineArray len bigger than 2 then it's a transaction
+      if(lineArray.length > 2) {
+        createTransaction(lineArray);
+      } else {
+        checkCommand(lineArray);
+      }
+      console.log(lineArray);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function insertInitialData() {
+  try {
+    let size = metadado.INITIAL.id.length;
     const insertvalues = 'INSERT INTO metadado (id, A, B) VALUES ($1, $2, $3)';
 
-    let values = [
-      metadado.INITIAL.id[0],
-      metadado.INITIAL.A[0],
-      metadado.INITIAL.B[0]
-    ];
+    for(let i = 0; i < size; i++) {
+      let values = [
+        metadado.INITIAL.id[i],
+        metadado.INITIAL.A[i],
+        metadado.INITIAL.B[i]
+      ];
 
-    await client.query(insertvalues, values)
-
-    values = [
-      metadado.INITIAL.id[1],
-      metadado.INITIAL.A[1],
-      metadado.INITIAL.B[1]
-    ];
-
-    await client.query(insertvalues, values)
-
-    const selectquery = 'SELECT * FROM metadado';
-
-    await client
-      .query(selectquery)
-      .then(res => console.log(res.rows))
-      .catch(e => console.error(e.stack))
-
+      await client.query(insertvalues, values)
+    }
 
   } catch (err) {
     console.log(err);
   }
+}
+
+async function createTransaction(lineArray) {
+  let idTupla = lineArray[0];
+  let id = lineArray[1];
+  let column = lineArray[2];
+  let oldValue = lineArray[3];
+  let newValue = lineArray[4];
+
+  // create a transaction
+  if (lineArray[0] === 'UPDATE') {
+    const updatequery = 'UPDATE metadado SET A = $1, B = $2 WHERE id = $3';
+
+    values = [
+      lineArray[1],
+      lineArray[2],
+      lineArray[3]
+    ];
+
+    client.query(updatequery, values)
+      .then(res => console.log(res.rows))
+      .catch(e => console.error(e.stack))
+  }
+}
+
+async function checkCommand(lineArray) {
 }
 
 readLog();
