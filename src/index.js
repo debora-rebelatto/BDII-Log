b5c7c6ffc940d4598eba3c31ab3c6eb7c663ceb4
larimones <larissamones12@gmail.com>
2022-12-14 22:43:23 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const fs = require('fs/promises');
-const metadado = require('./logFiles/metadado.json'); // pega o arquivo de metadado
-const client = require('./db');
+const express = require("express");
+const fs = require("fs/promises");
+const metadado = require("./logFiles/metadado.json");
+const client = require("./db");
 
 const app = express();
 const port = 4000;
 
-require('dotenv').config() // pega o arquivo oculto .env
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// cria uma rota para a aplicação que retorna o json com os dados
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const res = await getMetadata();
     return res;
@@ -21,149 +19,112 @@ app.get('/', async (req, res) => {
   }
 });
 
-// inicia o server node
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-// faz um select na tabela de metadado e retorna o resultado
-async function getMetadata() {
-  const selectquery = 'SELECT * FROM metadado';
-
-  return await client
-    .query(selectquery)
-    .then(res => console.log(res.rows))
-    .catch(e => console.error(e.stack))
-
-}
+  console.log(`App listening at http://localhost:${port}`);
+});
 
 async function readLog() {
   try {
-    // load entradaLog file
-    const data = await fs.readFile('./src/logFiles/entradaLog', { encoding: 'utf8' });
+    let ckpt = /ckpt/gi;
+    let commit = /commit/gi;
+    let start = /start/gi;
+    let crash = /crash/gi;
 
-    // faz split do arquivo em linhas
-    let lines = data.split('\r');
+    let checkpointArray = [];
+    let commitedTransactionArray = [];
+    let redoTransactionArray = [];
 
-    console.log("Inicia conexão com o banco de dados")
+    const data = await fs.readFile("./src/logFiles/entradaLog", {
+      encoding: "utf8",
+    });
 
+    if (!crash.exec(data)) return;
 
-    // conecta no banco de dados
-    await client.connect();
-
-    // cria a tabela de metadado
-    await createMetadadoTable();
-
-    // insere os dados iniciais
-    await insertInitialData();
-
-    console.log("Inserido dados iniciais")
-
-    await getMetadata();
-
-    console.log("Inicia leitura de log")
-
-    // revert o array de linhas
+    let lines = data.split("\r");
     lines = lines.reverse();
 
-    let transactionArray = [];
-    let commitedTransactionArray = [];
-    // percorre as linhas do arquivo
-    lines.forEach((line, index) => {
-      // verifica se a linha é valida
-      if(line === "" || line === "\r" || line === "\n") {
-        return;
-      }
+    console.log("Inicia conexão com o banco de dados");
+    await client.connect();
 
-      // transforma a linha em lowercase para comparar com os comandos
+    await createMetadadoTable();
+    await insertInitialData();
+    await getMetadata();
+
+    lines.forEach(async (line, index) => {
+      if (line === "" || line === "\r" || line === "\n") return;
+
       let lowercaseLine = line.toLowerCase();
 
-      let ckpt = /ckpt/gi;
-      let commit = /commit/gi;
-      let start = /start/gi;
-      let crash = /crash/gi;
+      if (ckpt.exec(lowercaseLine)) {
+        let lineArray = line.split(" ");
+        checkpointArray = lineArray[1].match(/\(([^)]+)\)/)[1].split(",");
+      } else if (commit.exec(lowercaseLine)) {
+        if (checkpointArray.length > 0) return;
 
-      if(ckpt.exec(lowercaseLine)) {
-        console.log("Inicia ckpt", line);
-        let lineArray = line.split(' ');
-        transactionArray = lineArray[1].match(/\(([^)]+)\)/)[1].split(',');
-
-        console.log("Transações que serão commitadas", transactionArray);
-
-      } else if(commit.exec(lowercaseLine)) {
-        console.log("Inicia commit", line);
-        let lineArray = line.split(' ');
+        let lineArray = line.split(" ");
         let transactionId = lineArray[1];
         transactionId = transactionId.substring(0, transactionId.length - 1);
 
-        console.log(transactionId)
-
-        // console.log("Transações que serão commitadas", transactionArray);
-
-        // put transaction in commitedTransactionArray
         commitedTransactionArray.push(transactionId);
-
-        console.log("Transações commitadas", commitedTransactionArray);
-
-        // if transactionId is in transactionArray it has redo
-        if(transactionArray.includes(transactionId)) {
-          console.log("Transação tem redo", transactionId);
-        } else {
-          console.log("Transação não tem redo", transactionId);
-        }
-
-      } else if(start.exec(lowercaseLine)) {
-        let lineArray = line.split(' ');
-        let transactionId = lineArray[1];
-        console.log("Inicia transação", line);
-
-      } else if(crash.exec(lowercaseLine)) {
-        console.log("Linha tem crash", line);
-      } else {
-        console.log("Linha tem transação", line);
       }
-
     });
+
+    checkpointArray.forEach((transactionId, index) => {
+      if (commitedTransactionArray.includes(transactionId)) {
+        redoTransactionArray.push(transactionId);
+      }
+    });
+
+    console.log("========= Finaliza leitura de log =========");
+    console.log("checkpointArray", checkpointArray);
+    console.log("Transações commitadas", commitedTransactionArray);
+    console.log("Transações que serão redo", redoTransactionArray);
+    console.log("===========================================");
   } catch (err) {
     console.log(err);
   }
 }
 
 async function createMetadadoTable() {
-  // faz drop na tabela caso ela exista
-  client.query('DROP TABLE IF EXISTS metadado');
+  client.query("DROP TABLE IF EXISTS metadado");
 
-  // cria a tabela metadado
   client
-    .query('CREATE TABLE IF NOT EXISTS metadado (id VARCHAR(255) PRIMARY KEY, A INTEGER, B INTEGER)')
-    .then(res => console.log("CREATED TABLE metadado"))
-    .catch(e => console.error(e.stack))
+    .query(
+      "CREATE TABLE IF NOT EXISTS metadado (id VARCHAR(255) PRIMARY KEY, A INTEGER, B INTEGER)"
+    )
+    .then((res) => console.log("CREATED TABLE metadado"))
+    .catch((e) => console.error(e.stack));
 }
 
 async function insertInitialData() {
   try {
-    // verifica o tamanho do array de dados iniciais
     let size = metadado.INITIAL.id.length;
 
-    // cria a query de inserção
-    const insertvalues = 'INSERT INTO metadado (id, A, B) VALUES ($1, $2, $3)';
+    const insertvalues = "INSERT INTO metadado (id, A, B) VALUES ($1, $2, $3)";
 
-    // percorre o array de dados iniciais e insere no banco
-    for(let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
       let values = [
         metadado.INITIAL.id[i],
         metadado.INITIAL.A[i],
-        metadado.INITIAL.B[i]
+        metadado.INITIAL.B[i],
       ];
 
-      // insere os dados no banco
-      await client.query(insertvalues, values)
+      await client.query(insertvalues, values);
     }
 
+    console.log("Inserido dados iniciais");
   } catch (err) {
     console.log(err);
   }
 }
 
-// inicia a leitura do arquivo de log
+async function getMetadata() {
+  const selectquery = "SELECT * FROM metadado";
+
+  return await client
+    .query(selectquery)
+    .then((res) => console.log(res.rows))
+    .catch((e) => console.error(e.stack));
+}
+
 readLog();
